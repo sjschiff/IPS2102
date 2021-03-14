82b21a7e983f426a1286ce605f6ef2093c764a8e
Sam Schiffer <sjschiff@ncsu.edu>
2021-03-14 19:06:04 -0400

    Create PatientProfDB skeleton

--- src/PatientProfDB.java
+++ src/PatientProfDB.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class PatientProfDB {
+    private int numPatient;
+    private int currentPatientIndex;
+    private String filename;
+    private ArrayList<PatientProf> patientList;
+
+    public PatientProfDB(String filename){
+
+    }
+
+    public void insertNewProfile(PatientProf patient){
+
+    }
+
+    public boolean deleteProfile(String adminID, String lastName){
+        return false;
+    }
+
+    public PatientProf findProfile(String adminID, String lastName){
+        return null;
+    }
+
+    public PatientProf findFirstProfile(){
+        return null;
+    }
+
+    public PatientProf findNextProfile(){
+        return null;
+    }
+
+    public void writeAllPatientProf(String filename){
+
+    }
+
+    public void initializeDatabase(String filename){
+
+    }
+
 }
