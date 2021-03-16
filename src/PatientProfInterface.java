21d8715c6bd929c2e2a483d7b0a7545001d820d0
Sam Schiffer <sjschiff@ncsu.edu>
2021-03-16 15:09:08 -0400

    Create PatientProfInterface skeleton

--- src/PatientProfInterface.java
+++ src/PatientProfInterface.java
@@ -1,5 +1,47 @@
 public class PatientProfInterface {
-    public static void main(String[] args){
+
+    PatientProfInterface(String filename){
 
     }
+
+    public void getUserChoice(){
+
+    }
+
+    public void deletePatientProfile(){
+
+    }
+
+    public void findPatientProfile(){
+
+    }
+
+    public void updatePatientProf(){
+
+    }
+
+    public void displayPatientProf(){
+
+    }
+
+    public void displayAllPatientProf(){
+
+    }
+
+    public void writeToDB(){
+
+    }
+
+    public void initDB(){
+
+    }
+
+    public PatientProf createNewPatientProf(){
+        return null;
+    }
+
+    public MedCond createNewMedCond(){
+        return null;
+    }
+
 }
