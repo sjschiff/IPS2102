98402684b73e25b2d2fc41b611ab3f79495ddfc6
Sam Schiffer <sjschiff@ncsu.edu>
2021-04-10 18:59:04 -0400

    Create MainMenu layout

--- src/IPSGUI/MainMenu.java
+++ src/IPSGUI/MainMenu.java
@@ -1,18 +1,61 @@
 package IPSGUI;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-
-public class MainMenu extends JFrame{
+public class MainMenu extends JFrame implements ActionListener{
     //JFrame f;
+    JLabel title;
+    JRadioButton create, delete, update, find, display;
+    JButton select;
+
     public MainMenu(){
-        JButton b = new JButton("Click");
-        b.setBounds(350,460,100,40);
+        title = new JLabel("Integrated Patient System");
+        title.setFont(new Font("Serif", Font.PLAIN, 25));
 
-        add(b);
+        create = new JRadioButton("Create Profile");
+        delete = new JRadioButton("Delete Profile");
+        update = new JRadioButton("Update Profile");
+        find = new JRadioButton("Find/Display Profile");
+        display = new JRadioButton("Display All Profiles");
 
-        setSize(800, 1000);
-        setLayout(null);
+        select = new JButton("Select");
+
+        title.setHorizontalAlignment(JLabel.CENTER);
+
+        create.setHorizontalAlignment(JLabel.CENTER);
+        delete.setHorizontalAlignment(JLabel.CENTER);
+        update.setHorizontalAlignment(JLabel.CENTER);
+        find.setHorizontalAlignment(JLabel.CENTER);
+        display.setHorizontalAlignment(JLabel.CENTER);
+
+        select.setHorizontalAlignment(JLabel.CENTER);
+
+        ButtonGroup choices = new ButtonGroup();
+
+        choices.add(create);
+        choices.add(delete);
+        choices.add(update);
+        choices.add(find);
+        choices.add(display);
+
+        add(title);
+
+        add(create);
+        add(delete);
+        add(update);
+        add(find);
+        add(display);
+
+        add(select);
+
+        setSize(400, 500);
+        setLayout(new GridLayout(7,0));
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+
     }
 
     public static void main(String[] args){
