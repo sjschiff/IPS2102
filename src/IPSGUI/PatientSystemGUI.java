c25a4d612770b233c7bb0b2cab1923e0eb53c16a
Sam Schiffer <sjschiff@ncsu.edu>
2021-04-12 22:48:34 -0400

    Finish startUp implementation to handle invalid files

--- src/IPSGUI/PatientSystemGUI.java
+++ src/IPSGUI/PatientSystemGUI.java
@@ -21,21 +21,38 @@ public class PatientSystemGUI extends JFrame implements ActionListener {
         startUp();
     }
 
+    // Function that handles the startup of the program, selecting the file to run with and launching the main menu
+    public void startUp(){
+
+        // Create a new object to choose a file
+        JFileChooser fc = new JFileChooser();
+        boolean valid = false;
+        // Keep trying to choose and open a file until it works with a valid file or user hits CANCEL
+        while(!valid) {
+            int i = fc.showOpenDialog(this);
+            // Open file branch
+            if (i == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                filePath = file.getPath();
+                try {
+                    database = new PatientProfDB(filePath);
+                    database.initializeDatabase(filePath);
+                    valid = true;
+                } catch (RuntimeException e){
+                    new ErrorBox(e.getMessage());
+                }
+            }
+            // Cancel branch
+            else if (i == JFileChooser.CANCEL_OPTION){
+                valid = true;
+            }
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()== mainMenu.getSelect()){
             System.out.println("Select Clicked");
-        }
-    }
-
-    public void startUp(){
-        JFileChooser fc = new JFileChooser();
-        int i = fc.showOpenDialog(this);
-        if(i==JFileChooser.APPROVE_OPTION){
-            File file = fc.getSelectedFile();
-            filePath = file.getPath();
-            database = new PatientProfDB(filePath);
-            database.initializeDatabase(filePath);
         }
     }
 
